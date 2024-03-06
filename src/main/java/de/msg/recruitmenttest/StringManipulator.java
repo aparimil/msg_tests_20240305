a48eedac7875c35f75613fa1313bf6cbfffd87d9
aparimil <162443013+aparimil@users.noreply.github.com>
2024-03-06 16:40:54 +0530

    Update StringManipulator.java
    Methods implementation completed

--- src/main/java/de/msg/recruitmenttest/StringManipulator.java
+++ src/main/java/de/msg/recruitmenttest/StringManipulator.java
@@ -1,25 +1,33 @@
 package main.java.de.msg.recruitmenttest;
-
+import java.util.*;
 public class StringManipulator {
 
     // Task 1.1
     // Write a method that reverses a given string.
     public String reverseString(String input) {
-        // TODO: Implement the logic for reversing the string
-        return null;
+        return new StringBuilder(input).reverse().toString();
     }
 
     // Task 1.2
     // Write a method that checks if a given string is a palindrome.
     public boolean isPalindrome(String input) {
-        // TODO: Implement the logic for checking palindrome
-        return false;
+        String reversed = new StringBuilder(input).reverse().toString();
+        return input.equals(reversed);
     }
 
     // Task 1.3
     // Write a method that finds the first non-repeated character in a given string.
     public char firstNonRepeatedCharacter(String input) {
-        // TODO: Implement the logic for finding the first non-repeated character
+        Map<Character, Integer> charCountMap = new HashMap<>();
+        for (char ch : input.toCharArray()) {
+            charCountMap.put(ch, charCountMap.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : input.toCharArray()) {
+            if (charCountMap.get(ch) == 1) {
+                return ch;
+            }
+        }
         return '\0';
     }
 }
